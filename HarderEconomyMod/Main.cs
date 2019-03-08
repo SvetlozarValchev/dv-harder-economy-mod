43b46d4f95ca031021cbb53e5cb35bbead39f3af
Svetlozar Valchev <cbethax@gmail.com>
2019-03-08 02:47:09 +0200

    initial

---
+++ HarderEconomyMod/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using UnityModManagerNet;
+using Harmony12;
+using UnityEngine;
+
+namespace HarderEconomyMod
+{
+    public class Main
+    {
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            // Something
+            return true; // If false the mod will show an error.
+        }
+    }
+
+    [HarmonyPatch(typeof(JobPaymentCalculator), "CalculateHaulPayment")]
+    class JobPaymentCalculator_CalculateHaulPayment_Patch
+    {
+        static void Postfix(ref float __result)
+        {
+            __result = Mathf.Round(__result * 0.25f);
+        }
+    }
+
+    [HarmonyPatch(typeof(JobPaymentCalculator), "CalculateShuntingPayment")]
+    class JobPaymentCalculator_CalculateShuntingPayment_Patch
+    {
+        static void Postfix(ref float __result)
+        {
+            __result = Mathf.Round(__result * 0.25f);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# dv-harder-economy-mod
